687b835483bda35de7182c5d9b5f407e5a2835b1
Owen Rubel <orubel@gmail.com>
2017-08-03 13:51:53 -0700

    saving changes for index page

--- public/html/assets/js/account/index.js
+++ public/html/assets/js/account/index.js
@@ -19,12 +19,11 @@
 				dataType:"text json",
 				success: function(json) {
 				    var out = `
-                        <section class="col-lg-12">
                             <div class="row">
                                 <div class="form-group">
                                     <label class="label col col-2">Account</label>
                                     <div class="col col-10">
-                                        <select id='account' class='form-control' onChange='getAccount(this.value,this.options[this.selectedIndex].text)'>
+                                        <select id='account' class='form-control' onChange='getApplications(this.value,this.options[this.selectedIndex].text)'>
                                         <option>None Chosen</option>`;
 
 					$.each(json, function(key,val){
@@ -34,38 +33,25 @@
 					                    </select>
                                     </div>
                                 </div>
-                            </div>
-                        </section>`;
+                            </div>`;
 
 					document.getElementById("accounts").innerHTML = out;
 					document.getElementById("acct_title").innerHTML = "Accounts";
-
-                    //var element = document.getElementById('eventType');
-                    //element.value = id;
-                    //getReports(id,element.options[element.selectedIndex].text);
-
 				},
                error: function(jqXHR, textStatus, errorThrown) {
 				    var out = `
-                        <section class='col-lg-12'>
+
                             <div class='row'>
                                 <div class='form-group'>
                                     <label class='label col col-2'>Account Name</label>
-                                    <div class='col col-10'>
+                                    <div class='col col-8'>
                                     <input type='text' class='form-control' id='acctName'>
                                     </div>
-                                </div>
-                            </div>
-                        </section>
-                        <section class='col-lg-12'>
-                            <div class='row'>
-                                <div class='form-group'>
-                                    <div class='col col-12'>
+                                    <div class='col col-2'>
                                         <button type='submit' class='btn-u btn-u-blue' onclick='createAccount()'>Submit</button>
                                     </div>
                                 </div>
-                            </div>
-                        </section>`;
+                            </div>`;
 
 					document.getElementById("accounts").innerHTML = out;
 					document.getElementById("acct_title").innerHTML = "Create Account";
@@ -88,11 +74,81 @@
                 }),
                 dataType: 'json',
 				success: function(json) {
+				    var out = `
+                            <div class="row">
+                                <div class="form-group">
+                                    <label class="label col col-2">Account</label>
+                                    <div class="col col-10">
+                                        <select id='account' class='form-control' onChange='getApplications(this.value,this.options[this.selectedIndex].text)'>
+                                        <option>None Chosen</option>`;
 
-                    window.location.reload();
+					$.each(json, function(key,val){
+                    	    out += "<option value='"+val.acct+"'>"+val.acctName+"</option>";
+					});
+					out += `
+					                    </select>
+                                    </div>
+                                </div>
+                            </div>`;
+
+					document.getElementById("accounts").innerHTML = out;
+					document.getElementById("acct_title").innerHTML = "Accounts";
+
 				},
 				error: function(err) {
 					console.log("error",err);
+				}
+			});
+	}
+
+	function getApplications(val,acctName){
+			$.ajax({
+				type:'GET',
+				url: window.url+"/v0.1/application/list?acct="+val,
+                crossDomain: true,
+                cache:false,
+                headers:{
+                    'Content-Type':'application/json',
+                    'Authorization':'Bearer '+window.token.access_token
+                },
+                dataType: 'json',
+				success: function(json) {
+				    var out = `
+                            <div class="row">
+                                <div class="form-group">
+                                    <label class="label col col-2">Application</label>
+                                    <div class="col col-10">
+                                        <select id='app' class='form-control' onChange='showApplication(this.value,this.options[this.selectedIndex].text)'>
+                                        <option>None Chosen</option>`;
+
+					$.each(json, function(key,val){
+                    	    out += "<option value='"+val.id+"'>"+val.appName+"</option>";
+					});
+					out += `
+					                    </select>
+                                    </div>
+                                </div>
+                            </div>`;
+
+					document.getElementById("apps").innerHTML = out;
+					document.getElementById("acct_title").innerHTML = "Accounts";
+				},
+				error: function(err) {
+				    var out = `
+                            <div class='row'>
+                                <div class='form-group'>
+                                    <label class='label col col-2'>Application Name</label>
+                                    <div class='col col-8'>
+                                    <input type='text' class='form-control' id='appName'>
+                                    </div>
+                                    <div class='col col-2'>
+                                        <button type='submit' class='btn-u btn-u-blue' onclick='createApplication()'>Submit</button>
+                                    </div>
+                                </div>
+                            </div>`;
+
+					document.getElementById("apps").innerHTML = out;
+					document.getElementById("acct_title").innerHTML = "Accounts (Create Application)";
 				}
 			});
 	}
