420f1d260e18b6123c3da1ef16676f4b46dbf33d
Owen Rubel <orubel@gmail.com>
2025-01-27 11:23:39 -0800

    adding stats reporting. Initial stat is working

--- public/html/assets/js/stats.js
+++ public/html/assets/js/stats.js
@@ -1,133 +1,88 @@
-
-    function initStats(){
-			$.ajax({
-				type:'GET',
-				url: window.url+"/v1.0/stat/show",
-				crossDomain: true,
-                cache:false,
-                async:true,
-                contentType: 'application/json',
-                xhrFields:{
-                    withCredentials: false
-                },
-				beforeSend: function(request){
-					request.setRequestHeader('Authorization','Bearer '+window.token.access_token);
-				},
-				headers:{
-					'Content-Type':'application/json'
-				},
-				dataType:"text json",
-				success: function(json) {
-				    var out = "";
-                    var count = 1;
-
-					var userLabel = []
-					var userData = []
-
-                    var statLabel = []
-                    var statData = []
-
-                    var codeLabel = []
-                    var codeData = []
-
-                    if(json){
-
-                        for (var k in json['userTotals']){
-                            userLabel.push(k);
-                            userData.push(json['userTotals'][k]);
-                        }
-
-                        for (var k in json['apiTotals']){
-                            statLabel.push(k);
-                            statData.push(json['apiTotals'][k]);
-                        }
-
-                        for (var k in json['codeTotals']){
-                            codeLabel.push(k);
-                            codeData.push(json['codeTotals'][k]);
-                        }
-
-					}
-
-					var ctx1 = document.getElementById('bar-chart').getContext('2d');
-					new Chart(ctx1, {
-                          type: 'line',
-                          data: {
-                            labels: statLabel,
-                            datasets: [
-                              {
-                                label: "Daily API Statistics",
-                                fill:true,
-                                backgroundColor: ["#3e95cd"],
-                                data: statData
-                              }
-                            ]
-                          },
-                          options: {
-                            legend: { display: true },
-                            title: {
-                              display: true,
-                              text: 'Daily API Report'
-                            }
-                          }
-                     });
-
-
-					var ctx2 = document.getElementById('pie-chart1').getContext('2d');
-					new Chart(ctx2, {
-                          type: 'pie',
-                          data: {
-                            labels: userLabel,
-                            datasets: [
-                              {
-                                label: "Top 5 Users",
-                                backgroundColor: ['#36a2eb','#cc65fe','#3e95cd','#ff6384'],
-                                data: userData
-                              }
-                            ]
-                          },
-                          options: {
-                            title: {
-                              display: true,
-                              text: 'Top 5 Users'
-                            }
-                          }
-                     });
-
-
-                    var ctx3 = document.getElementById('pie-chart2').getContext('2d');
-                    new Chart(ctx3, {
-                           type: 'pie',
-                           data: {
-                             labels: codeLabel,
-                             datasets: [
-                               {
-                                 label: "Top 5 Return Codes",
-                                 backgroundColor: ['#36a2eb','#cc65fe','#3e95cd','#ff6384'],
-                                 data: codeData
-                               }
-                             ]
-                           },
-                           options: {
-                             title: {
-                               display: true,
-                               text: 'Top 5 Return Codes'
-                             }
-                           }
-                      });
+var stats = null
 
 
 
-				},
-               error: function(jqXHR, textStatus, errorThrown) {
-                    console.log('error');
-                    if (jqXHR.statusText =='abort') { return; }
-                    console.log(jqXHR.status);
-                    //console.log(jqXHR.getAllResponseHeaders());
-                    console.log(textStatus);
-                    alert(jqXHR.responseText);
-                },
-			});
-	}
+function outputTotals(responseText) {
+    alert("calling output properties (properties.js)");
+    alert(responseText)
+            var cookies = document.cookie.split(';');
+            for(var i=0 ; i < cookies.length ; ++i) {
+                var pair = cookies[i].trim().split('=');
+                console.log("pair:"+pair);
+                if(pair[0]=='JSESSIONID'){
+                    console.log(pair[0]+"/"+pair[1]);
+                }
+            }
+
+
+
+
+
+       var out = "";
+        document.getElementById("apidocs").innerHTML = null
+
+        var json = JSON.parse(responseText);
+
+        var props = Object.keys(json);
+alert("props output (properties.js) : "+props);
+
+        out += `
+        <h2 id="props">Properties</h2>
+        <div class="table-responsive"><table class="table">
+        <thead>
+        <tr>
+        <th class='col-md-3'>Name</th>
+        <th  class='col-md-9'>Value</th>
+        </tr></thead><tbody>`;
+
+
+        out += `
+        <tr><td>Name</td><td><code>`+json['name']+`</code></td></tr>
+        <tr><td>Server Type</td><td><code>`+json['serverType']+`</code></td></tr>
+        <tr><td>Documentation</td><td><code>`+json['documentationUrl']+`</code></td></tr>
+        <tr><td>IOState Directory</td><td><code>`+json['iostateDir']+`</code></td></tr>
+        <tr><td>Encoding</td><td><code>`+json['encoding']+`</code></td></tr>
+        <tr><td>Supported Formats</td><td><code>`+json['supportedFormats']+`</code></td></tr>
+        <tr><td>Batching?</td><td><code>`+json['batchingEnabled']+`</code></td></tr>
+        <tr><td>Chaining?</td><td><code>`+json['chainingEnabled']+`</code></td></tr>
+        <tr><td>Chaining Limit</td><td><code>`+json['apichainLimit']+`</code></td></tr>
+        <tr><td>Reserved Uris</td><td><code>`+json['reservedUris']+`</code></td></tr>
+        <tr><td>Login Attempt Limit</td><td><code>`+json['attempts']+`</code></td></tr>
+        <tr><td>Public Endpoints</td><td><code>`+json['publicEndpoint']+`</code></td></tr>`;
+
+        out += `</tbody></table></div><br><br>`;
+
+
+        out += `
+        <h2 id="props">Security Properties</h2>
+        <div class="table-responsive"><table class="table">
+        <thead>
+        <tr>
+        <th class='col-md-3'>Name</th>
+        <th  class='col-md-9'>Value</th>
+        </tr></thead><tbody>`;
+
+
+alert("out2:"+out)
+
+/*
+        out += `<tr><td>CORS Network Groups</td><td>`;
+
+        var grps = Object.keys(json['security']['corsNetworkGroups']);
+
+
+        out += `<div class="table-responsive"><table class="table" style='margin:0px;'>`;
+        for (const grp of grps) {
+            out += `<tr><td class='col-md-3'  style='border: none;'><code>`+grp+`</code></th><td  class='col-md-9' style='border: none;'>`+JSON.stringify(json['security']['corsNetworkGroups'][grp])+`</th></tr>`;
+        }
+        out += `</table></div>`;
+        out += `</td></tr>`;
+*/
+
+        out += `</tbody></table></div><br><br>`;
+alert("out3 : "+out)
+
+         document.getElementById("apidocs").innerHTML = out;
+}
 
 
